eab4e56a36a5bf76959a6831ecd7e0dfa0a0bd72
rosdyana <rosdyana.kusuma@gmail.com>
2018-07-06 10:20:38 +0800

    + Add portal

---
+++ Assets/Script/AI/GamePortal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePortal : MonoBehaviour
+{
+    public GameObject PortalOut;
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        collision.gameObject.transform.position = PortalOut.transform.position;
+    }
+}
